edb9470033cbba8c03c5027e8c2ce86a9bc39e5d
beacah <beacmrdcm@gmail.com>
2021-08-19 21:22:21 -0300

    pagina web

--- pages/index.js
+++ pages/index.js
@@ -49,13 +49,37 @@ export default function Home() {
           <a>
 
           <ul className ={styles.botaoCadastro}>
-          <button type="submit">Cadastrar-se</button>
+          <a>Cadastrar-se</a>
           </ul>
 
           </a>
 
         </div>
       </main>
+    
+    <div className={styles.forms}>
+
+    <form name="contact" method="POST" data-netlify="true">
+      <p className = {styles.name}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" />
+      </p>
+      <p>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" name="email" />
+      </p>
+      <p>
+        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message"></textarea>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+
+    </form>
+
+    </div>
+
 </div>
   )
 }
